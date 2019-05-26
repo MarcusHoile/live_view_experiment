598a030cc368100636827023987a0befc6d3d14c
marcushoile <marcus.hoile@gmail.com>
2019-05-27 09:24:53 +1000

    remove cruft

--- lib/live_view_experiment_web/live/dashboard_view.ex
+++ lib/live_view_experiment_web/live/dashboard_view.ex
@@ -24,15 +24,6 @@ defmodule LiveViewExperimentWeb.DashboardView do
     """
   end
 
-  def handle_event("districts:" <> district_id, value, socket) do
-    IO.puts("EVENT received")
-    {:noreply, assign(socket, status: "district id: #{district_id} is #{value}")}
-  end
-
-  def handle_event("kill", _value, socket) do
-    {:noreply, assign(socket, status: ":boom:")}
-  end
-
   def handle_info(%{payload: payload}, %{assigns: %{districts: districts}} = socket) do
     {:noreply, assign(socket, districts: Enum.concat(districts, [payload]))}
   end
